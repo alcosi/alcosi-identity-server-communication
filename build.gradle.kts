704a37d6ddca2ada996b780cff67ee3ee9de15b0
BreninSul <42633769+BreninSul@users.noreply.github.com>
2024-07-11 19:58:31 +0200

    Update application version in build.gradle.kts
    The build.gradle.kts file has been updated to reflect the new application version. The version string "1.21-$env" has been updated to "1.23-$env" marking an incremental change in the software release.

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,7 +40,7 @@ val javaVersion = JavaVersion.VERSION_21
 val env = "RELEASE"
 
 group = "com.alcosi"
-version = "1.21-$env"
+version = "1.23-$env"
 java.sourceCompatibility = javaVersion
 
 idea {
